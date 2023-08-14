627e674caae2ba133fcbf2240d8cef8cacb35463
Manakuremati <74898405+Manakuremati@users.noreply.github.com>
2023-08-14 21:29:08 +0200

    Update update-cloudflare-dns.sh

--- update-cloudflare-dns.sh
+++ update-cloudflare-dns.sh
@@ -10,19 +10,19 @@ fi
 LOG_FILE=${parent_path}'/update-cloudflare-dns.log'
 
 ### Write last run of STDOUT & STDERR as log file and prints to screen
-exec > >(tee $LOG_FILE) 2>&1
+exec > >(tee "$LOG_FILE") 2>&1
 echo "==> $(date "+%Y-%m-%d %H:%M:%S")"
 
 ### Validate if config-file exists
 
 if [[ -z "$1" ]]; then
-  if ! source ${parent_path}/update-cloudflare-dns.conf; then
+  if ! source "${parent_path}"/update-cloudflare-dns.conf; then
     echo 'Error! Missing configuration file update-cloudflare-dns.conf or invalid syntax!'
     exit 0
   fi
 else
-  if ! source ${parent_path}/"$1"; then
-    echo 'Error! Missing configuration file '$1' or invalid syntax!'
+  if ! source "${parent_path}"/"$1"; then
+    echo "Error! Missing configuration file "$1" or invalid syntax!"
     exit 0
   fi
 fi
@@ -64,10 +64,10 @@ for record in "${dns_records[@]}"; do
   if [ "${proxied}" == "false" ]; then
     ### Check if "nslookup" command is present
     if which nslookup >/dev/null; then
-      dns_record_ip=$(nslookup ${record} | awk '/Address/ { print $2 }' | sed -n '2p')
+      dns_record_ip=$(nslookup "${record}" | awk '/Address/ { print $2 }' | sed -n '2p')
     else
       ### if no "nslookup" command use "host" command
-      dns_record_ip=$(host -t A ${record} | awk '/has address/ { print $4 }' | sed -n '1p')
+      dns_record_ip=$(host -t A "${record}" | awk '/has address/ { print $4 }' | sed -n '1p')
     fi
 
     if [ -z "$dns_record_ip" ]; then
@@ -83,16 +83,16 @@ for record in "${dns_records[@]}"; do
       -H "Authorization: Bearer $cloudflare_zone_api_token" \
       -H "Content-Type: application/json")
     if [[ ${dns_record_info} == *"\"success\":false"* ]]; then
-      echo ${dns_record_info}
+      echo "${dns_record_info}"
       echo "Error! Can't get dns record info from Cloudflare API"
       exit 0
     fi
-    is_proxed=$(echo ${dns_record_info} | grep -o '"proxied":[^,]*' | grep -o '[^:]*$')
-    dns_record_ip=$(echo ${dns_record_info} | grep -o '"content":"[^"]*' | cut -d'"' -f 4)
+    is_proxed=$(echo "${dns_record_info}" | grep -o '"proxied":[^,]*' | grep -o '[^:]*$')
+    dns_record_ip=$(echo "${dns_record_info}" | grep -o '"content":"[^"]*' | cut -d'"' -f 4)
   fi
 
   ### Check if ip or proxy have changed
-  if [ ${dns_record_ip} == ${ip} ] && [ ${is_proxed} == ${proxied} ]; then
+  if [ ${dns_record_ip} == "${ip}" ] && [ "${is_proxed}" == "${proxied}" ]; then
     echo "==> DNS record IP of ${record} is ${dns_record_ip}", no changes needed.
     continue
   fi
@@ -104,13 +104,13 @@ for record in "${dns_records[@]}"; do
     -H "Authorization: Bearer $cloudflare_zone_api_token" \
     -H "Content-Type: application/json")
   if [[ ${cloudflare_record_info} == *"\"success\":false"* ]]; then
-    echo ${cloudflare_record_info}
+    echo "${cloudflare_record_info}"
     echo "Error! Can't get ${record} record information from Cloudflare API"
     exit 0
   fi
 
   ### Get the dns record id from response
-  cloudflare_dns_record_id=$(echo ${cloudflare_record_info} | grep -o '"id":"[^"]*' | cut -d'"' -f4)
+  cloudflare_dns_record_id=$(echo "${cloudflare_record_info}" | grep -o '"id":"[^"]*' | cut -d'"' -f4)
 
   ### Push new dns record information to Cloudflare API
   update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$cloudflare_dns_record_id" \
@@ -118,7 +118,7 @@ for record in "${dns_records[@]}"; do
     -H "Content-Type: application/json" \
     --data "{\"type\":\"A\",\"name\":\"$record\",\"content\":\"$ip\",\"ttl\":$ttl,\"proxied\":$proxied}")
   if [[ ${update_dns_record} == *"\"success\":false"* ]]; then
-    echo ${update_dns_record}
+    echo "${update_dns_record}"
     echo "Error! Update failed"
     exit 0
   fi
