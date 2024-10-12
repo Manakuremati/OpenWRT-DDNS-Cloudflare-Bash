d60940935d2a35ca15b65dd9782868b38c342798
Manakuremati <74898405+Manakuremati@users.noreply.github.com>
2024-10-12 22:20:50 +0200

    Update update-cloudflare-dns.sh

--- update-cloudflare-dns.sh
+++ update-cloudflare-dns.sh
@@ -2,15 +2,8 @@
 
 ###  Create .update-cloudflare-dns.log file of the last run for debug
 parent_path="$(dirname "${BASH_SOURCE[0]}")"
-FILE=${parent_path}/update-cloudflare-dns.log
-if ! [ -x "$FILE" ]; then
-  touch "$FILE"
-fi
-
-LOG_FILE=${parent_path}'/update-cloudflare-dns.log'
 
 ### Write last run of STDOUT & STDERR as log file and prints to screen
-exec > >(tee "$LOG_FILE") 2>&1
 echo "$(date "+%Y-%m-%d %H:%M:%S")"
 logger -p notice "$(date "+%Y-%m-%d %H:%M:%S")"
 
@@ -31,9 +24,9 @@ else
 fi
 
 ### Check validity of "ttl" parameter
-if [ "${ttl}" -lt 60 ] || [ "${ttl}" -gt 7200 ] && [ "${ttl}" -ne 1 ]; then
-  echo "Error! ttl out of range (60-7200) or not set to 1"
-  logger -p notice "Error! ttl out of range (60-7200) or not set to 1"
+if [ "${ttl}" -lt 60 ] || [ "${ttl}" -gt 86400 ] && [ "${ttl}" -ne 1 ]; then
+  echo "Error! ttl out of range (60-86400) or not set to 1"
+  logger -p notice "Error! ttl out of range (60-86400) or not set to 1"
   exit
 fi
 
@@ -59,8 +52,8 @@ ip=$(ifstatus wan |  jsonfilter -e '@["ipv4-address"][0].address')
 	logger -p notice "Error! IP Address returned was invalid!"
     exit 0
   fi
-echo "IP is: $ip"
-logger -p notice "IP is: $ip"
+echo "WAN IP is: $ip"
+logger -p notice "WAN IP is: $ip"
 
 ### Build coma separated array fron dns_record parameter to update multiple A records
 IFS=',' read -d '' -ra dns_records <<<"$dns_record,"
@@ -68,26 +61,8 @@ unset 'dns_records[${#dns_records[@]}-1]'
 declare dns_records
 
 for record in "${dns_records[@]}"; do
-  ### Get IP address of DNS record from 1.1.1.1 DNS server when proxied is "false"
-  if [ "${proxied}" == "false" ]; then
-    ### Check if "nslookup" command is present
-    if which nslookup >/dev/null; then
-      dns_record_ip=$(nslookup "${record}" | awk '/Address/ { print $2 }' | sed -n '2p')
-    else
-      ### if no "nslookup" command use "host" command
-      dns_record_ip=$(host -t A "${record}" | awk '/has address/ { print $4 }' | sed -n '1p')
-    fi
 
-    if [ -z "$dns_record_ip" ]; then
-      echo "Error! Cannot resolve ${record}"
-	  logger -p notice "Error! Cannot resolve ${record}"
-      exit 0
-    fi
-    is_proxed="${proxied}"
-  fi
-
-  ### Get the dns record id and current proxy status from Cloudflare API when proxied is "true"
-  if [ "${proxied}" == "true" ]; then
+  ### Get the dns record id and current proxy status from Cloudflare API
     dns_record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$record" \
       -H "Authorization: Bearer $cloudflare_zone_api_token" \
       -H "Content-Type: application/json")
@@ -100,7 +75,7 @@ for record in "${dns_records[@]}"; do
     fi
     is_proxed=$(echo "${dns_record_info}" | grep -o '"proxied":[^,]*' | grep -o '[^:]*$')
     dns_record_ip=$(echo "${dns_record_info}" | grep -o '"content":"[^"]*' | cut -d'"' -f 4)
-  fi
+    dns_record_id=$(echo "${dns_record_info}" | grep -o '"id":"[^"]*' | cut -d'"' -f4)
 
   ### Check if ip or proxy have changed
   if [ ${dns_record_ip} == "${ip}" ] && [ "${is_proxed}" == "${proxied}" ]; then
@@ -112,23 +87,8 @@ for record in "${dns_records[@]}"; do
   echo "DNS record of ${record} is: ${dns_record_ip}. Trying to update..."
   logger -p notice "DNS record of ${record} is: ${dns_record_ip}. Trying to update..."
 
-  ### Get the dns record information from Cloudflare API
-  cloudflare_record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$record" \
-    -H "Authorization: Bearer $cloudflare_zone_api_token" \
-    -H "Content-Type: application/json")
-  if [[ ${cloudflare_record_info} == *"\"success\":false"* ]]; then
-    echo "${cloudflare_record_info}"
-	logger -p notice "${cloudflare_record_info}"
-    echo "Error! Cannot get ${record} record information from Cloudflare API"
-	logger -p notice "Error! Cannot get ${record} record information from Cloudflare API"
-    exit 0
-  fi
-
-  ### Get the dns record id from response
-  cloudflare_dns_record_id=$(echo "${cloudflare_record_info}" | grep -o '"id":"[^"]*' | cut -d'"' -f4)
-
   ### Push new dns record information to Cloudflare API
-  update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$cloudflare_dns_record_id" \
+  update_dns_record=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dns_record_id" \
     -H "Authorization: Bearer $cloudflare_zone_api_token" \
     -H "Content-Type: application/json" \
     --data "{\"type\":\"A\",\"name\":\"$record\",\"content\":\"$ip\",\"ttl\":$ttl,\"proxied\":$proxied}")
@@ -140,8 +100,6 @@ for record in "${dns_records[@]}"; do
     exit 0
   fi
 
-  echo "Success!"
-  logger -p notice "Success!"
-  echo "$record DNS Record updated to: $ip, ttl: $ttl, proxied: $proxied"
-  logger -p notice "$record DNS Record updated to: $ip, ttl: $ttl, proxied: $proxied"
+  echo "Success $record DNS Record updated to: $ip, ttl: $ttl, proxied: $proxied"
+  logger -p notice "Success $record DNS Record updated to: $ip, ttl: $ttl, proxied: $proxied"
 done
